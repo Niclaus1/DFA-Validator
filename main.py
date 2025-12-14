f64bc674acad7bc600d2308604ac24e5c40b727d
Niclaus1 <rendonniclaus111@gmail.com>
2025-12-14 13:48:28 +0800

    Added PsuedoCode

---
+++ main.py
@@ -0,0 +1,25 @@
+# DFA - Validator
+
+# states ask the number of q
+# alphabet ask the allowed inputs
+# transistions set transitions
+
+"""PsuedoCode
+1. Ask for the allowed input pattern
+    - Allowed only are 1,0 
+    - if 1* means any number of 1
+    - if 0* means any number of 0
+2. Automatically Generates a states
+3. create dictionary for each state
+    q# : (input1: q#, input0: q#)
+    - Print the dictionaries
+4. In a While Loop, ask user to input some 1s & 0s
+5. Ask user if he wants to stop
+6. If yes
+    - print all the inputs with each corresponding reponse (valid | invalid)
+    - end program
+7. Else
+    - continue
+"""
+
+
