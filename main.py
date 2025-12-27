534c8fdf23e92b5a3966c0f3e9b7ff2dec539c64
Niclaus1 <rendonniclaus111@gmail.com>
2025-12-27 17:08:38 +0800

    Finished Code

--- main.py
+++ main.py
@@ -1,131 +1,72 @@
 import re
-# DFA - Validator
 
-# states ask the number of q
-# alphabet ask the allowed inputs
-# transistions set transitions
-
-"""PsuedoCode
-1. Ask for the allowed input pattern 
-    - Allowed only are 1,0 
-    - if 1* means any number of 1
-    - if 0* means any number of 0
-    - if e1 means even number of 1
-    - if o1 means odd number of 1
-    - if e0 means even number of 0
-    - if o0 means off number of 0
-2. Generate a states as dictionary
-3. create dictionary for each state
-    q# : (input1: q#, input0: q#)
-    - Print the dictionaries
-4. In a While Loop, ask user to input some 1s & 0s
-5. Ask user if he wants to stop
-6. If yes
-    - print all the inputs with each corresponding reponse (valid | invalid)
-    - end program
-7. Else
-    - continue
-"""
-def Main():
-    userInput = input("""
-            1  = single 1
-            0  = single 0
-            1* = infinite number of 1
-            0* = infinite number of 0
-            e1 = even number of 1
-            e0 = even number of 0
-            o1 = odd number of 1
-            o0 = odd number of 0
-            *Use "," for each Unique Input*
-    Create your Pattern: """)
-
-
-    #Create State Dictionaries
-
-    splitInput = userInput.split(",")
-    countInput = len(splitInput)
-
-    #Create a Dictionary - keys based on len(list)
-    #Create a unique code per pattern
-    stateDict = {}
-    codePattern = ""
-    for i in range (countInput):
-        match splitInput[i]:
-            case "1":
-                stateDict["q" + str(i)] = "Single 1"
-                codePattern = codePattern + "0" #Ucode
-            case "0":
-                stateDict["q" + str(i)] = "Single 0"
-                codePattern = codePattern + "1" #Ucode
-            case "1*":
-                stateDict["q" + str(i)] = "Infinite 1s"
-                codePattern = codePattern + "2" #Ucode
-            case "0*":
-                stateDict["q" + str(i)] = "Infinite 0s"
-                codePattern = codePattern + "3" #Ucode
-            case "e1":
-                stateDict["q" + str(i)] = "Even 1"
-                codePattern = codePattern + "4" #Ucode
-            case "e0":
-                stateDict["q" + str(i)] = "Even 0"
-                codePattern = codePattern + "5" #Ucode
-            case "o1":
-                stateDict["q" + str(i)] = "Odd 1"
-                codePattern = codePattern + "6" #Ucode
-            case "o0":
-                stateDict["q" + str(i)] = "Odd 0"
-                codePattern = codePattern + "7" #Ucode
-    print("")
-    print(f"Each status of the Pattern is: {stateDict}")
-    print("")
-    print(f"Unique Code of the Pattern is: {codePattern}")
-    print("")
-    return codePattern
-    
-def UserInput():
-    #4. In a While Loop, ask user to input some 1s & 0s
-    #thoughts: creating a unique code per each pattern, where when we input the data, 
-    #          it translates into a code for easy checking
-    userInputs = []
-    status = True
-    while status:
-        inputPattern = input(
-    """ 
-    Input your Patterns!
-    Type stop to exit-- 
+def get_pattern_code():
+    print("""
+    OPTIONS:
+    1  = single 1
+    0  = single 0
+    1* = infinite 1s (0 or more)
+    0* = infinite 0s (0 or more)
+    e1 = consecutive even pair of 1s (11, 1111...)
+    e0 = consecutive even pair of 0s (00, 0000...)
+    o1 = consecutive odd 1s (1, 111...)
+    o0 = consecutive odd 0s (0, 000...)
     """)
-        userInputs.append(inputPattern)
-        
-        if inputPattern == "stop":
-            userInputs.pop(-1)
-            status = False
-        
-    return userInputs
-
-def DataChecking():
-    uniqueCode = Main()
-    listOfInputs = UserInput()
     
-    regex_map = {
-        "0": "1",       # Single 1
-        "1": "0",       # Single 0
-        "2": "1*",      # Infinite 1s (zero or more)
-        "3": "0*",      # Infinite 0s (zero or more)
-        "4": "(11)*",   # Even 1s (consecutive blocks of 1s)
-        "5": "(00)*",   # Even 0s
-        "6": "1(11)*",  # Odd 1s
-        "7": "0(00)*"   # Odd 0s
+    user_input = input("Create your Pattern (use ',' to separate): ")
+    parts = user_input.split(",")
+    
+    # Build the regex string directly (Skip the intermediate 'code numbers')
+    regex_pattern = "^" # Start of string
+    
+    pattern_map = {
+        "1": "1",
+        "0": "0",
+        "1*": "1*",
+        "0*": "0*",
+        "e1": "(11)*",
+        "e0": "(00)*",
+        "o1": "1(11)*",
+        "o0": "0(00)*"
     }
-    
-    pattern = "^"
-    for char in uniqueCode:
-        pattern += regex_map.get(char, "")
-    pattern += "$"
-        
-    for i in listOfInputs:
-        if re.fullmatch(pattern, i):
-            print(f"Input'{i}': VALID")
+
+    for part in parts:
+        part = part.strip() # Remove accidental spaces
+        if part in pattern_map:
+            regex_pattern += pattern_map[part]
         else:
-            print(f"Input'{i}': INVALID")
-            
-DataChecking()
+            print(f"Warning: '{part}' is not a valid code. Skipping.")
+
+    regex_pattern += "$" # End of string
+    return regex_pattern
+
+def get_test_strings():
+    inputs = []
+    print("\nType strings to test (type 'stop' to finish):")
+    while True:
+        val = input("> ")
+        if val.lower() == "stop":
+            break
+        inputs.append(val)
+    return inputs
+
+def main():
+    # 1. Get the Rules
+    final_regex = get_pattern_code()
+    print(f"\nGenerated Regex Engine: {final_regex}")
+    
+    # 2. Get the Test Data
+    test_strings = get_test_strings()
+    
+    # 3. Run the Validator
+    print("-" * 20)
+    for string in test_strings:
+        if re.fullmatch(final_regex, string):
+            print(f"'{string}' -> VALID")
+        else:
+            print(f"'{string}' -> INVALID")
+    print("-" * 20)
+
+# Run the program
+
+main()
